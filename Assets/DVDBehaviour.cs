c704d4b34a21275cf71362e139d1ef5cc14afe5a
JinMoshchenko <jinmoshchenko@gmail.com>
2020-11-19 22:50:54 +0200

    PogChamp Update

--- Assets/DVDBehaviour.cs
+++ Assets/DVDBehaviour.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class DVDBehaviour : MonoBehaviour
 {
-    [SerializeField]
     private Rigidbody2D RB;
 
     public float XPush;
@@ -18,6 +17,8 @@ public class DVDBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+		RB = GetComponent<Rigidbody2D>();
+
         RB.velocity = new Vector2(XPush, YPush);
     }
 
@@ -27,8 +28,19 @@ public class DVDBehaviour : MonoBehaviour
         
     }
 
-    void ChangeColor()
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "ScreenEdge")
+		{
+			Debug.Log("Touchy");
+
+			ChangeColor();
+		}
+	}
+
+	void ChangeColor()
     {
+		Again:
         int RandomColor = Random.Range(0, Colors.Length);
         if (LastColor != RandomColor)
         {
@@ -37,22 +49,7 @@ public class DVDBehaviour : MonoBehaviour
         }
         else
         {
-            ChangeColor();
-        }
-    }
-
-
-
-  
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "ScreenEdge")
-        {
-            Debug.Log("Touchy");
-
-            ChangeColor();
-
-
+			goto Again;
         }
     }
 }
